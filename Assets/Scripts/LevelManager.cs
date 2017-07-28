84d5f0ccb2d8d5500a1501fcf310e194c4dc7274
Avishek's Macbook <avishek.ahmed@gmail.com>
2017-07-27 22:59:45 -0400

    Add Start Menu + Button + Gaze + Action

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNextScene(){
+		int currentScene = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentScene + 1);
+	}
+
+	public void Quit(){
+		Debug.Log ("Game Will Quit");
+		Application.Quit ();
+	}
+}

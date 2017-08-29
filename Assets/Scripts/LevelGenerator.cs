10025cff5c15280a70a207408c1bd33bea54eb14
Avishek's Macbook <avishek.ahmed@gmail.com>
2017-08-28 21:55:59 -0400

    level generator + tree generator

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public GameObject []prefabs;
+	float maxZ = 500;
+
+	void Start () {
+		for (int i = 0; i < maxZ; i += 10) {
+			CreateLand (Random.Range (0, 3), i);
+		}
+	}
+
+	void CreateLand(int landIndex,float z){
+		GameObject land = Instantiate (prefabs [landIndex]);
+		land.transform.parent = transform;
+		land.transform.Translate (new Vector3 (0, 0, z));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

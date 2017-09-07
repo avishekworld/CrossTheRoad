db104040b5af2bc3d3a49acbe5539c3c978bd124
Avishek's Macbook <avishek.ahmed@gmail.com>
2017-09-06 22:41:18 -0400

    add vehicle and movement

---
+++ Assets/VechicleMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VechicleMovement : MonoBehaviour {
+
+	public float velocity = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (-velocity * Time.deltaTime, 0, 0);
+	}
+}

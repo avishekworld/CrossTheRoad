c4123c4f101c9be1b798f24ef5c5aa65d442bad3
Avishek's Macbook <avishek.ahmed@gmail.com>
2017-09-28 22:12:53 -0400

    game over menu + rotate with camera

---
+++ Assets/Scripts/MenuFollewer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFollewer : MonoBehaviour {
+
+	//Camera camera;
+	void Start () {
+		//camera = transform.parent.GetComponentInChildren<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera camera = transform.parent.GetComponentInChildren<Camera> ();
+		Vector3 projectedLookDirection = Vector3.ProjectOnPlane (camera.transform.forward, Vector3.up);
+		Quaternion targetRotation =  Quaternion.LookRotation (projectedLookDirection);
+		transform.rotation = targetRotation;
+	}
+}

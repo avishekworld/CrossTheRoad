b6a44c7903c5c1e8efd9bab3a1c334072740ec35
Avishek's Macbook <avishek.ahmed@gmail.com>
2017-10-04 22:24:20 -0400

    game over condition implements

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour {
+
+	CollisionObserver collisionObserver;
+
+	void Start () {
+		collisionObserver = FindObjectOfType<CollisionObserver> ();	
+	}
+	
+	public void OnCollisionEnter(Collision collision){
+		collisionObserver.OnCollision ();
+	}
+}
---
+++ Assets/Scripts/CollisionObserver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionObserver : MonoBehaviour {
+
+	public GameObject gameOverMenuCanvas;
+
+	public void OnCollision(){
+		gameOverMenuCanvas.SetActive (true);
+	}
+}
